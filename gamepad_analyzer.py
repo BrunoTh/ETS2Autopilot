c01bbb408dfada8781db46923b86db94e206fea8
BrunoTh <BrunoTh@users.noreply.github.com>
2017-06-30 09:20:38 +0200

    started creating a gamepad analyzer for buttons and axes

---
+++ gamepad_analyzer.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+pygame.joystick.init()
+
+joystick_count = pygame.joystick.get_count()
+if joystick_count == 0:
+    print("No joysticks found.")
+    exit(0)
+
+for i in range(joystick_count):
+    print("%d - %s" % (i, pygame.joystick.Joystick(i).get_name()))
+
+try:
+    joystick_id = int(input("Choose a joystick: "))
+except ValueError:
+    print("Incorrect input. Only numbers are allowed.")
+    exit(1)
+
+if joystick_id < joystick_count:
+    print("Joystick ID is invalid.")
+    exit(0)
+
+joystick = pygame.joystick.Joystick(joystick_id)
+num_buttons = joystick.get_numbuttons()
+num_axes = joystick.get_numaxes()
+
+while(True):
+    pygame.event.pump()
+    # joystick.get_axis(i)
+    # joystick.get_button(i)

b37f39b4c7ec593953ff47c8efc8ba7e0bdd52a5
Bruno <BrunoTh@users.noreply.github.com>
2017-07-02 11:33:41 +0200

    finished small analyzer script

--- gamepad_analyzer.py
+++ gamepad_analyzer.py
@@ -17,15 +17,22 @@ except ValueError:
     print("Incorrect input. Only numbers are allowed.")
     exit(1)
 
-if joystick_id < joystick_count:
+if joystick_id > joystick_count:
     print("Joystick ID is invalid.")
     exit(0)
 
 joystick = pygame.joystick.Joystick(joystick_id)
+joystick.init()
 num_buttons = joystick.get_numbuttons()
 num_axes = joystick.get_numaxes()
 
 while(True):
     pygame.event.pump()
-    # joystick.get_axis(i)
-    # joystick.get_button(i)
+
+    for i in range(num_buttons):
+        if joystick.get_button(i) == 1:
+            print("Button %d is pressed." % i)
+
+    for i in range(num_axes):
+        if abs(joystick.get_axis(i)) > 0.3:
+            print("Axis %d is in use." % i)

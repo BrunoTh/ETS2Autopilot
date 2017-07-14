92eb0fa42896bd3d82139e88b3a19893e8fe4ed2
BrunoTh <BrunoTh@users.noreply.github.com>
2017-07-14 11:11:20 +0200

    finished base functionality

--- UI/database.py
+++ UI/database.py
@@ -1,57 +1,28 @@
 import sqlite3
 
-"""
-Image:
-INSERT INTO image VALUES (?,...);
-DELETE FROM image WHERE filename=?;
-UPDATE image SET maneuver=? WHERE filename=?;
-
-Sequence:
-INSERT INTO sequence (country) VALUES (?);
-DELETE FROM sequence WHERE id=?;
-UPDATE sequence SET country=?/type=? WHERE id=?;
-
-Country:
-INSERT INTO country VALUES (?);
-SELECT id FROM country WHERE code=?;
-
-
-get_country_id(code) - Creates or selects country and returns cid
-get_country_list() - Returns list with all country codes and ids
-
-add_image(filename, ...)
-set_image_maneuver(filename or filename-range)
-delete_image(filename)
-get_image_list(sequence) - Returns list with all images in a sequence
-
-add_sequence(country, type=-1)
-update_sequence(country, type)
-delete_sequence(id)
-get_sequence_list() - Returns list with all sequences
-"""
-
 
 class Database(object):
     def __init__(self):
-        sql_create_tables = """CREATE TABLE IF NOT EXISTS settings (
+        sql_create_tables = list()
+        sql_create_tables.append("""CREATE TABLE IF NOT EXISTS settings (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    key VARCHAR(45) PRIMARY KEY,
+                                    key VARCHAR(45) UNIQUE,
                                     value TEXT NOT NULL
-                                );
+                                );""")
                                 
-                                CREATE TABLE IF NOT EXISTS country (
+        sql_create_tables.append("""CREATE TABLE IF NOT EXISTS country (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     code VARCHAR(3) UNIQUE
-                                );
+                                );""")
                                 
-                                CREATE TABLE IF NOT EXISTS sequence (
+        sql_create_tables.append("""CREATE TABLE IF NOT EXISTS sequence (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     country INT,
                                     type INT DEFAULT -1,
                                     FOREIGN KEY(country) REFERENCES country(id)
-                                );
+                                );""")
                                 
-                                CREATE TABLE IF NOT EXISTS image (
+        sql_create_tables.append("""CREATE TABLE IF NOT EXISTS image (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     filename VARCHAR UNIQUE,
                                     steering DOUBLE NOT NULL,
@@ -60,22 +31,25 @@ class Database(object):
                                     maneuver INTEGER NOT NULL,
                                     sequence INTEGER NOT NULL,
                                     FOREIGN KEY(sequence) REFERENCES sequence(id) ON DELETE CASCADE 
-                                );
-                            """
+                                );""")
 
         self.conn = sqlite3.connect('data.sqlite')
 
         try:
             c = self.conn.cursor()
-            c.execute(sql_create_tables)
+            for sql_statement in sql_create_tables:
+                c.execute(sql_statement)
         except Exception as e:
             print(e)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
 
     def execute(self, command, params=()):
         try:
             c = self.conn.cursor()
             c.execute(command, params)
-
+            self.conn.commit()
             if command.startswith("INSERT"):
                 return c.lastrowid
             else:
@@ -107,35 +81,88 @@ class Settings(object):
         self.db.execute("DELETE FROM settings WHERE key=?", (key,))
 
 
-class Sequence(object):
+class Data(object):
     def __init__(self):
         self.db = Database()
+        self.settings = Settings()
 
     def get_country_id(self, code):
+        """
+        Looks up the id of the given country code and creates a new entry if the code isn't in the table.
+        :param code: Country code
+        :return: country id
+        """
         cid = self.db.execute("SELECT id FROM country WHERE code=?", (code,))
         if len(cid) > 0:
             return cid[0][0]
         else:
             return self.db.execute("INSERT INTO country (code) VALUES (?)", (code,))
 
-    def add_sequence(self, type):
+    def get_country_list(self):
         """
-        Creates a new sequence in db
-        :return: ID of new sequence
+        :return: List with all country codes and ids
         """
-        settings = Settings()
-        country = settings.get_value("country")
-        cid = self.get_country_id(country)
-        id = self.db.execute("INSERT INTO sequence (country) VALUES (?)", (cid,))
-        return id
-
-
-class Image(object):
-    def __init__(self):
-        self.db = Database()
+        result = self.db.execute("SELECT id, code FROM country")
+        if type(result) == str and result.startswith("ERROR"):
+            return []
+        else:
+            return result
 
     def add_image(self, filename, steering, speed, throttle, maneuver, sequence):
         self.db.execute("INSERT INTO image VALUES (?,?,?,?,?,?)", (filename, steering, speed, throttle, maneuver, sequence,))
 
+    def set_image_maneuver(self, filename, maneuver):
+        self.db.execute("UPDATE image SET maneuver=? WHERE filename=?", (maneuver, filename,))
+
     def delete_image(self, filename):
         self.db.execute("DELETE FROM image WHERE filename=?", (filename,))
+
+    def get_image_list(self, sequence):
+        """
+        :param sequence: id of sequence
+        :return: List with all images of a sequence
+        """
+        result = self.db.execute("SELECT filename, steering, speed, throttle, maneuver FROM image WHERE sequence=?", (sequence,))
+        if type(result) == str and result.startswith("ERROR"):
+            return []
+        else:
+            return result
+
+    def add_sequence(self, country=None, road_type=-1):
+        """
+        Creates a new sequence in db
+        :return: ID of new sequence
+        """
+        if not country:
+            if not self.settings.get_value("country"):
+                country = "NULL"
+            else:
+                country = self.settings.get_value("country")
+        cid = self.get_country_id(country)
+        return self.db.execute("INSERT INTO sequence (country, type) VALUES (?, ?)", (cid, road_type,))
+
+    def update_sequence(self, sid, country=None, road_type=None):
+        """
+        :param sid: id of sequence you want to update
+        :param country: country code
+        :param road_type: road type
+        :return:
+        """
+        if country is not None:
+            cid = self.get_country_id(country)
+            self.db.execute("UPDATE sequence SET country=? WHERE id=?", (cid, sid,))
+        if road_type is not None:
+            self.db.execute("UPDATE sequence SET type=? WHERE id=?", (road_type, sid,))
+
+    def delete_sequence(self, sid):
+        self.db.execute("DELETE FROM sequence WHERE id=?", (sid,))
+
+    def get_sequence_list(self):
+        """
+        :return: List with all sequences
+        """
+        result = self.db.execute("SELECT id, country, type FROM sequence")
+        if type(result) == str and result.startswith("ERROR"):
+            return []
+        else:
+            return result
